24bb04c881e0e8a2939a99efc8e356e2e00846e7
Kamil <klarity0431@gmail.com>
2026-09-02 22:19:04 +0530

    Deleted all the older files

--- apps/web/src/app/map/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import dynamic from "next/dynamic";
-
-const MapView = dynamic(() => import("@/components/MapView").then((m) => m.MapView), { ssr: false });
-
-export default function MapPage() {
-  return <MapView />;
-}
